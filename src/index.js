7216a738d12671fb027979ee447b4c0ec33a4dee
Anna Sapegina <watochka@gmail.com>
2021-08-23 19:02:16 +0300

    feat: implement min,max, avg

--- src/index.js
+++ src/index.js
@@ -1,12 +1,15 @@
 
 exports.min = function min (array) {
-  return 0;
+    return (array || []).reduce((a, b)=> a < b ? a : b, 0);
 }
 
 exports.max = function max (array) {
-  return 0;
+    return (array || []).reduce((a, b) => a > b ? a : b, 0);
 }
 
 exports.avg = function avg (array) {
-  return 0;
+    if (typeof(array) === "undefined" || array.length === 0) {
+        return 0
+    }
+    return array.reduce((a, b) => a + b,  0) / array.length;
 }
